bef33f0c0b066ba4457f990cb0a270a4ac6a289c
CharlyCoderHouse <carlosdiblasi@gmail.com>
2022-08-24 21:20:21 -0300

    Trabajo Clase 3 NavBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,8 +1,33 @@
-import logo from '../logo.svg';
+import Logo_Puga from '../Logo_Puga.png';
 const NavBar = () => {
     return (
         <div>
-           <img src={logo} className="App-logo" alt="logo" />
+            <a href="#" >
+                <img src={Logo_Puga} className="imgLogo" alt="Joyeria_Puga" width="140" height="100"/>
+            </a>
+            <h1 class="titulos">Proyecto Tienda Joyería PUGA</h1>
+            <nav class="menuNav">
+                <ul class="menuPrincipal">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Quienes Somos</a></li>
+                    <li><a href="#">Promociones</a></li>
+                    <li><a href="#">Productos</a>
+                        <ul class="subMenuPrincipal">
+                            <li><a href="#">Anillos</a></li>
+                            <li><a href="#">Relojes</a></li>
+                            <li><a href="#">Accesorios</a></li>
+                            <li><a href="#">Marroquinería</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="pages/contacto.html">Contactanos</a></li>
+                    <li class="nav-item">
+                        <button type="button" className="btn">Inicie Sesión</button>                                               
+                        </li>
+                    <li class="nav-item">
+                        <button type="button" className="btn">Carrito</button> 
+                    </li>  
+                </ul>
+            </nav>
         </div>
     )
 }
