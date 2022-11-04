01701c199c6439e9e4ad5b30d4fd52d447a3ff30
CharlyCoderHouse <carlosdiblasi@gmail.com>
2022-11-03 21:57:18 -0300

    Se renovo proyecto en Firebase

--- src/index.js
+++ src/index.js
@@ -10,14 +10,23 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Your web app's Firebase configuration // proyecto anterior dado de baja por 30 días de prueba
+/* const firebaseConfig = {
   apiKey: "AIzaSyARwdTCwS2Fqwoi3Vm4o8FzuTsPX0PXRM0",
   authDomain: "coderhouse-ecommerce-8cff9.firebaseapp.com",
   projectId: "coderhouse-ecommerce-8cff9",
   storageBucket: "coderhouse-ecommerce-8cff9.appspot.com",
   messagingSenderId: "874816016995",
   appId: "1:874816016995:web:3707417b71344675dade56"
+}; */
+// nuevo proyecto para seguir en uso
+const firebaseConfig = {
+  apiKey: "AIzaSyBhUleviNO_0loZF8FQ-sNmDi9IPuaZ4ks",
+  authDomain: "coder-ecommerce-916ac.firebaseapp.com",
+  projectId: "coder-ecommerce-916ac",
+  storageBucket: "coder-ecommerce-916ac.appspot.com",
+  messagingSenderId: "994885313407",
+  appId: "1:994885313407:web:ad759786f9a558ee25f04a"
 };
 
 // Initialize Firebase
